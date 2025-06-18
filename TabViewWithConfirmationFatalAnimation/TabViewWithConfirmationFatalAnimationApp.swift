4640705e14119c702e92b886173f5e2903fad155
John Siracusa <siracusa@gmail.com>
2025-06-18 09:57:10 -0400

    Initial Commit

---
+++ TabViewWithConfirmationFatalAnimation/TabViewWithConfirmationFatalAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewWithConfirmationFatalAnimationApp.swift
+//  TabViewWithConfirmationFatalAnimation
+//
+//  Created by Developer on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TabViewWithConfirmationFatalAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
